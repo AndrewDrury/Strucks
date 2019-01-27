67399ebce6328d143fc3de5d61dd49bc99cb7aad
Aditya Dutta <aditya.dutta10@gmail.com>
2019-01-27 07:28:59 -0500

    app v1

--- app/src/main/java/com/example/strucks/FaceRecognition.java
+++ app/src/main/java/com/example/strucks/FaceRecognition.java
@@ -6,12 +6,14 @@ import android.graphics.Matrix;
 import android.graphics.Rect;
 import android.location.Location;
 import android.support.annotation.NonNull;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.telephony.SmsManager;
 import android.util.Log;
 
 import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
@@ -57,6 +59,7 @@ import android.util.Size;
 import android.util.SparseIntArray;
 import android.view.Surface;
 import android.view.TextureView;
+import android.view.View;
 import android.widget.Toast;
 
 import java.io.File;
@@ -175,6 +178,8 @@ public class FaceRecognition extends AppCompatActivity {
                 checkID();
             }
         }, 5, 5, TimeUnit.SECONDS);
+
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
     }
 
     private void checkID() {
@@ -221,6 +226,7 @@ public class FaceRecognition extends AppCompatActivity {
 
         if(currentID != initID && currentID != -1 && initID != -1) {
             Log.i("testing", "INTRUDER ALERT --------");
+            getCurrentLocation();
         } else {
             Log.i("testing", "All Good --------");
             initID = currentID;
@@ -533,7 +539,7 @@ public class FaceRecognition extends AppCompatActivity {
                         .setClassificationMode(FirebaseVisionFaceDetectorOptions.ACCURATE)
                         //.setLandmarkMode(FirebaseVisionFaceDetectorOptions.ALL_LANDMARKS)
                         .setClassificationMode(FirebaseVisionFaceDetectorOptions.ALL_CLASSIFICATIONS)
-                        .setMinFaceSize(0.15f)
+                        .setMinFaceSize(0.1f)
                         .enableTracking()
                         .build();
         // [END set_detector_options]
@@ -580,10 +586,6 @@ public class FaceRecognition extends AppCompatActivity {
 
                                                 Log.i("testing","faceID: " + currentID);
 
-                                            }
-                                            else{
-                                                //sending alert
-                                                getCurrentLocation();
                                             }
                                         }
                                         // [END get_face_info]
@@ -665,7 +667,7 @@ public class FaceRecognition extends AppCompatActivity {
     public void sendSMS() {
         SmsManager smsManager = SmsManager.getDefault();
         Log.i("log", "SMS is called");
-        smsManager.sendTextMessage(new String("6475348632"), null, message, null, null);
+        smsManager.sendTextMessage(new String("2263486765"), null, message, null, null);
         Toast.makeText(getApplicationContext(), "SMS sent.",
                 Toast.LENGTH_LONG).show();
     }
@@ -696,5 +698,9 @@ public class FaceRecognition extends AppCompatActivity {
                         }
                     }
                 });
+    }
+
+    public void emergencyClick(View view) {
+        getCurrentLocation();
     }
 }
